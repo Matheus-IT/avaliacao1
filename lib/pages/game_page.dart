b1e739fe6c2522ee3f937472f75b1a06a7e9b85c
matheus costa <matheuscosta.tech@gmail.com>
2022-06-10 19:10:29 -0300

    Info for AppBar

--- lib/pages/game_page.dart
+++ lib/pages/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prog4_avaliacao1/components/regular_text.dart';
 
 import '../components/accent_text.dart';
 
@@ -14,7 +15,13 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const AccentText(content: 'Jokenpô', fontSize: 46.0),
+        title: Row(
+          children: const [
+            AccentText(content: 'Jokenpô', fontSize: 46.0),
+            RegularText(content: 'P:1'),
+            RegularText(content: 'M:0'),
+          ],
+        ),
       ),
     );
   }
